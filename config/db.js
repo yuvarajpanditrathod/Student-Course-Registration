6ea4fbe5891f8aee7ada27f28e31d9e23f4afda6
Yuvaraj Pandit Rathod <106370935+yuvarajpanditrathod@users.noreply.github.com>
2025-05-08 13:47:23 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/student-course-registration');
+        await mongoose.connect('mongodb+srv://yuvrajrathod870:root@cluster0.abcd123.mongodb.net/student-registration?retryWrites=true&w=majority');
         console.log('MongoDB connected');
     } catch (err) {
         console.error(err);
